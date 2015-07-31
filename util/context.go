e1312de608e4dee659d532f01f0572e3498d2949
3onyc <3onyc@x3tech.com>
2015-07-31 23:23:46 +0200

    Add EventBus

--- util/context.go
+++ util/context.go
@@ -2,11 +2,13 @@ package util
 
 import (
 	"github.com/3onyc/3do/model"
+	"github.com/asaskevich/EventBus"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
 
 type Context struct {
+	Bus    *EventBus.EventBus
 	Router *mux.Router
 	DB     *sqlx.DB
 	Lists  *model.TodoListRepository
@@ -16,6 +18,7 @@ type Context struct {
 
 func NewContext(r *mux.Router, db *sqlx.DB) *Context {
 	return &Context{
+		Bus:    EventBus.New(),
 		Router: r,
 		DB:     db,
 		Lists:  model.NewTodoListRepository(db),
