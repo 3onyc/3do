74d770db95cc2388775ac0c6b4542cd6c3cb7090
3onyc <3onyc@x3tech.com>
2015-09-01 22:04:50 +0200

    Reload todo-list in todo-list route if relationships not loaded

--- frontend/app/routes/todo-list.js
+++ frontend/app/routes/todo-list.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model(params) {
-        return this.store.find('todo-list', params.id);
+    setupController(controller, model) {
+        if (!model) {
+            return controller.set('model', this.store.find('todo-list', model.id));
+        }
+
+        if (!model._internalModel ||
+            !model._internalModel._relationships ||
+            !model._internalModel._relationships.initializedRelationships
+        ) {
+            return controller.set('model', this.store.fetchById('todo-list', model.id));
+        }
+
+        var relationships = model._internalModel._relationships.initializedRelationships;
+        if (relationships.groups.canonicalState[0].dataHasInitialized) {
+            return;
+        }
+
+        return controller.set('model', this.store.fetchById('todo-list', model.id));
     }
 });
